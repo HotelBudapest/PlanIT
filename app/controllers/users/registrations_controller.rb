b09a7e930aed066f37efada4f7811f6e46c984c8
HotelBudapest <arianislam.010@gmail.com>
2024-07-17 15:01:18 +0600

    Added Further Convenience to log pages

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :combine_phone_number, only: [:create, :update]
+  before_action :split_phone_number, only: [:edit]
+  before_action :authenticate_user!, only: [:destroy]
 
   # GET /resource/sign_up
   # def new
@@ -59,4 +62,42 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  # 
+  #
+  
+  def destroy
+    @user = current_user
+
+    if @user.valid_password?(params[:user][:current_password])
+      super
+    else
+      flash[:alert] = "Current password is incorrect."
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+
+  def combine_phone_number
+    if params[:user][:phone_number].present? && params[:country_code].present?
+      params[:user][:phone_number] = "#{params[:country_code]}#{params[:user][:phone_number]}"
+    end
+  end
+
+  def split_phone_number
+    if resource.phone_number.present?
+      match = resource.phone_number.match(/^(\+\d{1,3})(\d+)$/)
+      if match
+        @country_code = match[1]
+        @local_phone_number = match[2]
+      else
+        @country_code = ''
+        @local_phone_number = resource.phone_number
+      end
+    else
+      @country_code = ''
+      @local_phone_number = ''
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   root 'home#index'
-  
+
   resources :events do
     member do
       post 'invite'
